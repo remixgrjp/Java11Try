419b837d40a6995bc690610bd995167ebe07623f
remix <http+github.com@remix.asia>
2022-10-08 17:55:04 +0900

    build error "incompatible types: possible lossy conversion from int to char" Cause unknown: Not use Java11 public static String java.lang.Character.toString( int codePoint )

--- app/src/main/java/asia/remix/java11try/MainActivity.java
+++ app/src/main/java/asia/remix/java11try/MainActivity.java
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity{
 	protected void onCreate( Bundle savedInstanceState ){
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_main );
+
+		int i = 0x20BB7;//surrogate pair Unicode
+		String str = java.lang.Character.toString( i );//Java 11
+
 	}
 }
